7e606d4421d2bfacf522cf12383c98d66869622d
crispybee <crispybee9@gmail.com>
2016-10-04 17:04:44 +0200

    included main snippet

--- PascalParser/PascalParser.cpp
+++ PascalParser/PascalParser.cpp
@@ -1,11 +1,26 @@
-// PascalParser.cpp : Defines the entry point for the console application.
-//
-
 #include "stdafx.h"
-
-
-int main()
+#include <fstream>
+#include <iostream>
+#include <string>
+#include "GoldCPP\MIMParser.h"
+using namespace GoldCPP;
+using namespace std;
+int _tmain(int argc, _TCHAR* argv[])
 {
-    return 0;
+	string egt_file = "pascal.egt";
+	string script_file = "pasprog.pas";
+	GPSTR_T output;
+	{
+		MIMParser parser(egt_file);
+		std::ifstream srcInput(script_file, std::ios::binary);
+		GPSTR_T srcStr((std::istreambuf_iterator<char>(srcInput)),
+			(std::istreambuf_iterator<char>()));
+		srcInput.close();
+		if (parser.Parse(srcStr, output))
+		{
+			cout << "Erfolg! Bitte Taste druecken." << endl;
+		}
+	}
+	std::getchar();
+	return 0;
 }
-
