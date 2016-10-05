35c00acc2e8992cf472eb13a02fe965123f8fa4c
crispybee <crispybee9@gmail.com>
2016-10-05 12:49:13 +0200

    made Pascal test program more readable

--- PascalParser/pasprog.pas
+++ PascalParser/pasprog.pas
@@ -8,29 +8,33 @@ begin
 	c := (a * b / 2 * (4 - 5)) + 20;
 	i := 1;
 	d := 0;
-	if a < b then a := b;
+	if a < b then
+		a := b;
 	if a < b + 2 then
 	begin
-	a := b;
-	b := c;
+		a := b;
+		b := c;
 	end
 	else
-	b := a;
-	while i < 30 do i := i + 1;
+		b := a;
+	while i < 30 do
+		i := i + 1;
 	repeat
-	i := i - 1;
+		i := i - 1;
 	until i = 0;
-	for i := 1 to 20 do a := a + i;
+	for i := 1 to 20 do
+		a := a + i;
 	while a < 1 + d do
 	begin
-	repeat
-	if b > 2 then
-	begin
-	for i := 1 to d+20 do a := a + i;
-	end
-	else
-	a := 3000;
-	until a > 2999;
-	i:=5;
+		repeat
+		if b > 2 then
+		begin
+			for i := 1 to d+20 do
+				a := a + i;
+		end
+		else
+			a := 3000;
+		until a > 2999;
+		i:=5;
 	end;
-	end.
+end.
